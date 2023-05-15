d5786de11f263f06a03a28944f3b680451591ef3
“harshaV87” <harsha.87b@gmail.com>
2023-05-15 11:43:33 -0400

    first commit

---
+++ Sources/CustomFrameworkPackage/SDK.swift
@@ -0,0 +1,14 @@
+//
+//  SDK.swift
+//  CustomFrameworkiOS
+//
+//  Created by Venkata harsha Balla on 5/15/23.
+//
+
+import Foundation
+
+public struct SDK {
+    public static func doSomeWork() {
+        print("do some work.....")
+    }
+}
